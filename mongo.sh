e7e8c1a118a14b1333dc65faa6e3397511c1247b
clutch <a32j.web+clutch@proton.me>
2026-07-16 18:45:09 +0100

    Create mongo.sh

---
+++ mongo.sh
@@ -0,0 +1,9 @@
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+
+sudo apt update
+sudo apt install -y mongodb-org
+
+sudo -u mongodb mongod --config /etc/mongod.conf --fork
